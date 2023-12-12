d45702909247f7ce10bef9e998faeda88af174ba
robertdjordanbitbucket <robertjo@blackmagicdesign.com>
2023-12-12 16:05:05 +1100

    reg decod development, seems to decode most regs

--- vhdl_tokeniser/reg_decode.py
+++ vhdl_tokeniser/reg_decode.py
@@ -18,11 +18,20 @@ def convert_vhdl_reg_to_comment(input_str, vhdl_obj):
             if "&" in from_to[1]:
                 packed_reg = from_to[1].split("&")
                 for reg in packed_reg:
-                    if '"' not in reg and 'x"' not in reg:
+                    if '"' in reg or "'" in reg:
+                        if "x" in reg:
+                            zeros = find_between(reg, '"','"')
+                            packed.append(int(len(zeros))*4)
+                        else:
+                            zeros = find_between(reg, '"','"')
+                            packed.append(int(len(zeros)))
+                    else:
                         if "(" in reg and ")" in reg:
                             reg = find_between(reg, "(", ")")
                         packed.append(reg.replace(";",""))
             else:
+                if "(" in from_to[1] and ")" in from_to[1]:
+                            from_to[1] = find_between(from_to[1], "(", ")")
                 packed.append(from_to[1].strip())
             match = re.search(r'x"([^"]*)"', from_to[0])
             if match:
@@ -32,22 +41,25 @@ def convert_vhdl_reg_to_comment(input_str, vhdl_obj):
                 str_out = ''
                 start_bit = 0
                 for pack in reversed(packed): # go from end to begining
-                    found_sig = False
-                    for signal in vhdl_obj.signal:
-                        if signal[0] == pack.strip():
-                            found_sig = True
-                            str_out =  f"{pack.strip()}({int(signal[2])+start_bit}:{start_bit}) " + str_out  # extract the width
-                            start_bit = start_bit + signal[2]
-                    if found_sig == False: # if the assignment wasnt a signal check if it was a port
-                        for signal in vhdl_obj.port:
+                    if isinstance(pack, int):
+                         start_bit = start_bit + pack
+                    else:
+                        found_sig = False
+                        for signal in vhdl_obj.signal:
                             if signal[0] == pack.strip():
                                 found_sig = True
-                                try:
-                                    str_out =  f"{pack.strip()}({int(signal[3])+start_bit}:{start_bit}) " + str_out  # extract the width
-                                    start_bit = start_bit + signal[3]
-                                except:
-                                    print("decode_error")
-                                
+                                str_out =  f"{pack.strip()}({int(signal[2])-1+start_bit}:{start_bit}) " + str_out  # extract the width
+                                start_bit = start_bit + signal[2] 
+                        if found_sig == False: # if the assignment wasnt a signal check if it was a port
+                            for signal in vhdl_obj.port:
+                                if signal[0] == pack.strip():
+                                    found_sig = True
+                                    try:
+                                        str_out =  f"{pack.strip()}({int(signal[3])-1+start_bit}:{start_bit}) " + str_out  # extract the width
+                                        start_bit = start_bit + signal[3] 
+                                    except:
+                                        print("decode_error")
+                                    
 
                 print(f"-- 0x{regnum}   R    {str_out}")
 
