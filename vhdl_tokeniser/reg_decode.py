33aad93fd80e3f18780ab83434eb07fd9a5cdcc6
robertdjordanbitbucket <robertjo@blackmagicdesign.com>
2023-12-15 13:50:27 +1100

    reg decode can now do headers or c++ header

--- vhdl_tokeniser/reg_decode.py
+++ vhdl_tokeniser/reg_decode.py
@@ -8,8 +8,13 @@ def find_between( s, first, last ):
     except ValueError:
         return ""
 
-def convert_vhdl_reg_to_comment(input_str, vhdl_obj):
+def convert_vhdl_reg_to_code(input_str, vhdl_obj, cpp_out = False):
     cpp_header = []
+    if cpp_out == True:
+        print("// Auto Generated C/C++ header file for register file")
+
+    else:
+        print("-- Auto Generated VHDL header file for register file")
     lower = input_str.lower()
     lines = lower.split(';')
     for line in lines:
@@ -41,6 +46,7 @@ def convert_vhdl_reg_to_comment(input_str, vhdl_obj):
                 # -- 0x0000   RW    Switcher video_format(4:0)
                 str_out = ''
                 start_bit = 0
+
                 for pack in reversed(packed): # go from end to begining
                     if isinstance(pack, int):
                          start_bit = start_bit + pack
@@ -53,8 +59,10 @@ def convert_vhdl_reg_to_comment(input_str, vhdl_obj):
                                 cpp_shift = start_bit
                                 start_bit = start_bit + signal[2] 
                                 # cpp_header.append([f"#define {pack}     0x{regnum}", f"#define {pack}_shift     {start_bit}"])
-                                print(f"#define {pack.strip()}     0x{regnum}")
-                                print(f"#define {pack.strip()}_shift     {cpp_shift}")
+                                if cpp_out == True:
+                                    print(f"#define {pack.strip()}     0x{regnum}")
+                                    print(f"#define {pack.strip()}_shift     {cpp_shift}")
+                                    print(f"#define {pack.strip()}_size      {signal[2]}")
                                 
                         if found_sig == False: # if the assignment wasnt a signal check if it was a port
                             for signal in vhdl_obj.port:
@@ -65,13 +73,18 @@ def convert_vhdl_reg_to_comment(input_str, vhdl_obj):
                                         cpp_shift = start_bit
                                         start_bit = start_bit + signal[3] 
                                         # cpp_header.append([f"#define {pack}     0x{regnum}", f"#define {pack}_shift     {start_bit}"])
-                                        print(f"#define {pack.strip()}     0x{regnum}")
-                                        print(f"#define {pack.strip()}_shift     {cpp_shift}")
+                                        if cpp_out == True:
+                                            print(f"#define {pack.strip()}     0x{regnum}")
+                                            print(f"#define {pack.strip()}_shift     {cpp_shift}")
+                                            print(f"#define {pack.strip()}_size      {signal[3]}")
                                     except:
                                         print("decode_error")
                                     
-
-                print(f"-- 0x{regnum}   R    {str_out}")
+                if cpp_out == False:
+                    print(f"-- 0x{regnum}   R    {str_out}")
+    if cpp_out == True:
+        print("// Macros for extracting and setting values based on the given specifications \n#define EXTRACT_BITS(value, shift, size) (((value) >> (shift)) & ((1 << (size)) - 1)) \n#define SET_BITS(value, shift, size, data) ((value) = ((value) & ~(((1 << (size)) - 1) << (shift))) | (((data) & ((1 << (size)) - 1)) << (shift)))")
+    
                 
 
 
